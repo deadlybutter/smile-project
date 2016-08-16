4ff10e840f0b57cfed388b24d6f8add3b58bda21
deadlybutter <thedeadlybutter@gmail.com>
2016-08-16 16:32:48 -0400

    Base markup

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -7,21 +7,3 @@
 // Gallery
 //  - Photo
 //  - Blog Entry
-
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={`container ${this.props.fullscreen ? '-fullscreen' : ''} ${this.props.name ? this.props.name : ''}`}>
-        <div className="wrapper">
-          {this.props.content}
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Container content={<h1>#HappinessIs</h1>} fullscreen="true" name="introduction" />, document.getElementById('header'));
