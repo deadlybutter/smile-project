0dfd733da96d891771660c15656d4157cf5b8dc1
deadlybutter <thedeadlybutter@gmail.com>
2016-08-17 12:59:49 -0400

    Lunch break over, commiting work

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
+
+const feed = require("feed-read");
 
 app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/feed', function (req, res) {
+  feed(process.env.FEED_URL || '', function(err, articles) {
+    if (err) {
+      console.err(err);
+      return;
+    }
+
+    res.json(articles.splice(0, 4));
+  });
 });
 
 app.listen(process.env.PORT, function () {
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -7,3 +7,20 @@
 // Gallery
 //  - Photo
 //  - Blog Entry
+
+const BlogPost = React.createClass({
+  render: function() {
+    return (
+      <div className="blog__item">
+
+      </div>
+    );
+  }
+});
+
+$(document).on('ready', function() {
+  console.log("Uhh");
+  $.get('/feed', function(data) {
+    console.log(data);
+  });
+});
