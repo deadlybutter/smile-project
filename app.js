e352e2c7d747a651440dbb01bc321e6ec737bff8
deadlybutter <thedeadlybutter@gmail.com>
2016-08-17 17:30:13 -0400

    90% of the feed done

--- app.js
+++ app.js
@@ -3,22 +3,35 @@ const app = express();
 app.use(express.static('public'));
 
 const feed = require("feed-read");
+let feedItems = [];
+const CronJob = require('cron').CronJob;
 
 app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get('/feed', function (req, res) {
+  res.json(feedItems);
+});
+
+function fetchOnboardData() {
+  console.info('Fetching feed...');
   feed(process.env.FEED_URL || '', function(err, articles) {
     if (err) {
-      console.err(err);
+      console.err('Fetch error: ', err);
       return;
     }
 
-    res.json(articles.splice(0, 4));
+    console.info('Feed fetched.');
+    feedItems = articles.splice(0, 4);
   });
-});
+}
 
 app.listen(process.env.PORT, function () {
-  console.info(`App listening on port ${process.env.PORT}!`);
+  console.info(`App listening on port ${process.env.PORT}.`);
+  fetchOnboardData();
+
+  new CronJob('*/30 * * * * *', function() {
+    fetchOnboardData();
+  }, null, true, 'America/Los_Angeles');
 });
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,26 +1,51 @@
-// V1
-// Container -fullscreen
-//  - Wrapper
-//
-// CTA Button
-//
-// Gallery
-//  - Photo
-//  - Blog Entry
-
 const BlogPost = React.createClass({
   render: function() {
+    const blogData = this.props.data;
+    const $blogData = $.parseHtml(blogData.content);
+    
     return (
       <div className="blog__item">
-
+        <p>post</p>
       </div>
     );
   }
 });
 
-$(document).on('ready', function() {
-  console.log("Uhh");
-  $.get('/feed', function(data) {
-    console.log(data);
-  });
+const BlogContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      items: []
+    }
+  },
+
+  componentWillMount: function() {
+    $.get('/feed', (data) => {
+      this.setState({
+        items: data
+      });
+    });
+  },
+
+  render: function() {
+    const items = this.state.items;
+    console.log(items);
+    if (!items) {
+      return;
+    }
+
+    return (
+      <div>
+        {items.map(function(item, index) {
+          return <BlogPost key={index} data={item} />;
+        })}
+      </div>
+    );
+  }
+});
+
+$(function() {
+  ReactDOM.render(
+    <BlogContainer />,
+    document.getElementById('jsx-blog')
+  );
 });
